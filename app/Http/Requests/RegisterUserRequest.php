0147bf939dd63761ad1fde4566f0b57cfb362a37
tikoot <tiko.otarashvili99@gmail.com>
2022-11-15 22:08:46 +0100

    feat:create register forom request

---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'username'              => 'required|regex:/^[a-z0-9]+$/u|min:3|max:15',
+            'email'                 => 'required|email',
+            'password'              => 'required|min:8|max:15',
+            'password_confirmation' => 'required|same:password',
+        ];
+    }
+}
