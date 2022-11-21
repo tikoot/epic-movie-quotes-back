0443e76e99cca21e539a0c69200cf147fac45dca
tikoot <tiko.otarashvili99@gmail.com>
2022-11-21 15:09:59 +0100

    feat: send password reset link to user

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -38,6 +39,24 @@ class AuthServiceProvider extends ServiceProvider
                 $url .= "{$key}={$param}&";
             }
             return (new MailMessage())->view('verification-verify', ['url' => $url]);
+        });
+
+        ResetPassword::toMailUsing(function ($notifiable, $token) {
+            $params = [
+                'token' => $token,
+                'email' => $notifiable->getEmailForPasswordReset(),
+            ];
+
+            $url = env('FRONT_APP') . '/reset-password?';
+
+            foreach ($params as $key => $param) {
+                $url .= "{$key}={$param}&";
+            }
+
+            return (new MailMessage())
+            ->view('password-reset', [
+                'url'   => $url,
+            ]);
         });
     }
 }
