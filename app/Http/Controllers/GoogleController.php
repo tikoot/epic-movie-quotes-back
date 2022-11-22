0decba5ea7b914c84e27086d724c5b532525a7cb
tikoot <tiko.otarashvili99@gmail.com>
2022-11-22 16:03:04 +0100

    feat: create callback method with jwt

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -21,4 +21,60 @@ class GoogleController extends Controller
         $url =  Socialite::driver('google')->stateless()->redirect()->getTargetUrl();
         return response()->json(['url' => $url]);
     }
+
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('google')->stateless()->user();
+
+        $userFind = User::where('google_id', $user->id)->first();
+
+        if (!$userFind) {
+            $userFind = User::create([
+            'username' => $user->name,
+            'email'    => $user->email,
+            'password' => $user->id,
+            'google_id' => $user->id,
+            'email_verified_at' =>  Carbon::now()->format('d-m-Y')
+            ]);
+
+            auth()->attempt(
+                [
+                    'email' => $user->email,
+                    'password' => $user->id
+                ]
+            );
+
+
+            $payload = [
+                'exp' => Carbon::now()->addSeconds(30)->timestamp,
+                'uid' => User::where('email', '=', $user->email)->first()->id,
+            ];
+
+            $jwt = JWT::encode($payload, config('auth.jwt_secret'), 'HS256');
+
+            $cookie = cookie("access_token", $jwt, 30, '/', config('auth.front_end_top_level_domain'), true, true, false, 'Strict');
+
+            return response()->json('success', 200)->withCookie($cookie);
+        } else {
+            auth()->attempt(
+                [
+                    'email' => $user->email,
+                    'password' => $user->id
+                ]
+            );
+
+
+            $payload = [
+                'exp' => Carbon::now()->addSeconds(30)->timestamp,
+                'uid' => User::where('email', '=', $user->email)->first()->id,
+            ];
+
+            $jwt = JWT::encode($payload, config('auth.jwt_secret'), 'HS256');
+
+            $cookie = cookie("access_token", $jwt, 30, '/', config('auth.front_end_top_level_domain'), true, true, false, 'Strict');
+
+            return response()->json('success', 200)->withCookie($cookie);
+        }
+        return response()->json('success', 200);
+    }
 }
