c0b4e1823a670a27d76e497b5de9bb1e501a193f
tikoot <tiko.otarashvili99@gmail.com>
2022-11-30 18:52:51 +0100

    feat:make movie tables translatable

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -5,12 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Translatable\HasTranslations;
 
 class Movie extends Model
 {
     use HasFactory;
+    use HasTranslations;
 
     protected $guarded = ['id'];
+
+    public $translatable = ['movie_name','director','description'];
 
     public function user(): BelongsTo
     {
