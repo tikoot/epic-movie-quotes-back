2104418abcb97dd22791947fc8d842df8519a45a
tikoot <tiko.otarashvili99@gmail.com>
2022-11-30 18:53:29 +0100

    feat:create movie controller with movie store method

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function store(Request $request)
+    {
+        $movie = new Movie();
+        $movie->user_id = $request->user_id;
+        $movie->thumbnail = $request->file('thumbnail')->store('thumbnails');
+        $movie->setTranslation('movie_name', 'en', $request->movie_name_en);
+        $movie->setTranslation('movie_name', 'ka', $request->movie_name_ka);
+        $movie->setTranslation('director', 'en', $request->director_en);
+        $movie->setTranslation('director', 'ka', $request->director_ka);
+        $movie->setTranslation('description', 'en', $request->description_en);
+        $movie->setTranslation('description', 'ka', $request->description_ka);
+        $movie->save();
+
+        return response()->json('Movie created Successfully');
+    }
+}
