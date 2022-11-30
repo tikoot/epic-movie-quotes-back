29bdaf71805d299b9104d1c03adeb1389bdacae4
tikoot <tiko.otarashvili99@gmail.com>
2022-11-30 18:50:04 +0100

    feat: create model and table for movies

---
+++ database/migrations/2022_11_26_154101_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->json('movie_name');
+            $table->json('director');
+            $table->json('description');
+            $table->string('thumbnail');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
