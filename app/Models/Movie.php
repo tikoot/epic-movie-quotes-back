cf3fd9e6aec98d366651877a9713a53890c34b5f
tikoot <tiko.otarashvili99@gmail.com>
2022-12-06 14:24:42 +0100

    feat: create quote model and eloquent relationships

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Spatie\Translatable\HasTranslations;
 
 class Movie extends Model
@@ -19,5 +20,10 @@ class Movie extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function quotes(): HasMany
+    {
+        return $this->hasMany(Quote::class);
     }
 }
---
+++ app/Models/Quote.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Translatable\HasTranslations;
+
+class Quote extends Model
+{
+    use HasFactory;
+    use HasTranslations;
+
+    protected $guarded = ['id'];
+
+    public $translatable = ['quote'];
+
+    public function movie(): BelongsTo
+    {
+        return $this->belongsTo(Movie::class);
+    }
+}
