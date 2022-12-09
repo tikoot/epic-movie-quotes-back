a3f7e9fb70685b802aa0b4ab9285de4f1a8e7b7a
tikoot <tiko.otarashvili99@gmail.com>
2022-12-09 11:37:01 +0100

    feat: create comments table model and controller

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Movie;
+use App\Models\Quote;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+}
