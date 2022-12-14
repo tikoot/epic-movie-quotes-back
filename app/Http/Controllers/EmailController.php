ea712f118e53778edc3bb738cd0671be7c05269e
tikoot <tiko.otarashvili99@gmail.com>
2022-12-14 23:32:01 +0100

    feat:create new email and send verification email to primary email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -14,4 +14,22 @@ use Illuminate\Support\Str;
 
 class EmailController extends Controller
 {
+    public function store(Request $request): JsonResponse
+    {
+        $email =  Email::create([
+            'email'    => $request->email,
+            'user_id' => $request->user_id,
+            'token' => Str::random(60)
+        ]);
+
+        $user= User::findOrFail($request->user_id);
+
+        Mail::to($request->primary)->send(new VerifyEmail($email));
+
+
+        return response()->json([
+            "message" => "Email created successfully!",
+            "success" => true
+        ]);
+    }
 }
